d1858f60856fb34beeec49ca2b63dd05e3049f8d
Alex DR <alexandre.devaux-riviere@epita.fr>
2024-05-26 02:39:57 +0200

    deleting padding for ascii representation

--- src/image_to_ascii/transform.py
+++ src/image_to_ascii/transform.py
@@ -28,17 +28,17 @@ def grayscale_image(image):
     characters = "".join([ASCII_CHARS[pixel//25] for pixel in duplicated_pixels])
     return characters"""
 
-def image_to_ascii(image, ascii_chars=DEFAULT_ASCII_CHARS):
-    """Convert the image to ASCII art."""
-    image = grayscale_image(image)
-    pixels = np.array(image)
+def sentencify(ascii_art_list: list, new_width):
+    pixel_count = len(ascii_art_list) * 2
+    ascii_image = "\n".join([ascii_art_list[index:(index+new_width)] for index in range(0, pixel_count, new_width)])
+    return ascii_image
 
+def image_to_ascii(image, ascii_chars=DEFAULT_ASCII_CHARS, size=40):
+    """Convert the image to ASCII art."""
+    pixels = image.getdata()
     pixel_range = len(ascii_chars) - 1
-    ascii_str = '\n'.join(
-        ''.join(ascii_chars[pixel_value * pixel_range // 255] for pixel_value in row)
-        for row in pixels
-    )
-    return ascii_str
+    characters = "".join([ascii_chars[pixel_value * pixel_range // 255]for pixel_value in pixels])
+    return sentencify(characters, size)
 
 def csv_save(file_name, data):
     with open(file_name, mode='w', newline='') as file:
@@ -46,50 +46,53 @@ def csv_save(file_name, data):
         writer.writerows(data)
 
 # %%
-def load_image(input_image_path):
+def load_image(input_image_path, no_pg_process):
     """Load an image from the specified path."""
-    try:
-        image = Image.open(input_image_path)
+    image = Image.open(input_image_path)
+    if not no_pg_process:
         image = remove_background(image)
-        return image
-    except FileNotFoundError:
-        raise FileNotFoundError(f"Error: The specified image file '{input_image_path}' was not found.")
-    except Exception as e:
-        raise Exception(f"Error: Failed to load image '{input_image_path}': {e}")
+
+    image = grayscale_image(image)
+
+    image_csv2 = np.array(image)
+    mask = image_csv2 > 10
+    image_csv2 = image_csv2[np.ix_(mask.any(1), mask.any(0))]
+
+    pil_image = Image.fromarray(image_csv2)
+    return pil_image
 
 # %%
 def generate_ascii_art(image, output_width, ascii_chars=DEFAULT_ASCII_CHARS):
     """Generate ASCII art from an image."""
     image = resize_image(image, output_width)
-    ascii_art = image_to_ascii(image, ascii_chars)
+    ascii_art = image_to_ascii(image, ascii_chars, output_width*2)
     return ascii_art
 
 # %%
 ascii_chars = ' .:-=+*#%@'
 
 def main(data_path, csv_ascii_30_path, csv_asccii_60_path):
-    ascii_30, ascii_60 = [], []
     for datasets_name in os.listdir(data_path):
         dataset_path = os.path.join(data_path, datasets_name)
 
         for class_ in os.listdir(dataset_path):
             class_path = os.path.join(dataset_path, class_)
-
+            ascii_30, ascii_60 = [], []
+            i = 0
             for sample in tqdm(os.listdir(class_path)):
                 sample_path = os.path.join(class_path, sample)
                 #print(f"Processing: Dataset {datasets_name}, Class {class_}, Sample {sample}")
-                try:
-                    input_image = load_image(sample_path)
-                except:
-                    print(sample_path, " is not a valid pathname to an image.")
-                    continue
+                input_image = load_image(sample_path, "no_bg" in sample_path)
 
-                ascii_30.append([generate_ascii_art(input_image, 20, ascii_chars), class_, sample])
+                ascii_30.append([generate_ascii_art(input_image, 25, ascii_chars), class_, sample])
                 ascii_60.append([generate_ascii_art(input_image, 40, ascii_chars), class_, sample])
 
-                csv_save(os.path.join(csv_ascii_30_path, f"ascii_30_{datasets_name}_{class_}.csv"), ascii_30)
-                csv_save(os.path.join(csv_asccii_60_path, f"ascii_60_{datasets_name}_{class_}.csv"), ascii_60)
-                return
+                if i == 5:
+                    break
+                i += 1
+            csv_save(os.path.join(csv_ascii_30_path, f"ascii_30_{datasets_name}dd_{class_}.csv"), ascii_30)
+            csv_save(os.path.join(csv_asccii_60_path, f"ascii_60_{datasets_name}dd_{class_}.csv"), ascii_60)
+            return
     print('===> Done <===')
 
 # %%
